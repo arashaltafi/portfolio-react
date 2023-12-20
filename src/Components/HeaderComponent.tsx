58c1fd8213fb20850416531f7ce337f2101dc594
آرش الطافي <0021328374@nahad.ir>
2023-12-20 14:39:13 +0330

    a

--- src/Components/HeaderComponent.tsx
+++ src/Components/HeaderComponent.tsx
@@ -2,6 +2,11 @@ import { Link } from "react-router-dom"
 
 const HeaderComponent = () => {
 
+    const handleClickPdf = (e: any) => {
+        e.preventDefault();
+        window.open("https://arashaltafi.ir/resume_en.pdf");
+    }
+
     return (
         <nav className='flex flex-row items-stretch justify-between py-16 px-20 lg:py-16 lg:px-24'>
             <div className="hidden sm:flex flex-row gap-x-8 items-center justify-center child:title">
@@ -11,7 +16,7 @@ const HeaderComponent = () => {
                 <Link className="hover:custom-animation hover:transition hover:duration-100 delay-100 py-8 px-4" to="/">Home</Link>
             </div>
             <div className="flex flex-row gap-8 items-center justify-center child:title">
-                <a href="https://arashaltafi.ir/resume_en.pdf">Arash Altafi</a>
+                <div onClick={(e) => handleClickPdf(e)}>Arash Altafi</div>
             </div>
         </nav>
     )
