c2ebd32c0a36faec08c276919d55e1c2c6f7d87c
Arash Altafi <arashaltafi1377@gmail.com>
2023-12-22 15:36:54 +0330

    a

--- src/App.tsx
+++ src/App.tsx
@@ -76,7 +76,7 @@ function App() {
   }, [])
 
   return (
-    <div className='relative overflow-hidden'>
+    <div className='relative overflow-hidden select-none'>
       <CircleFollowCursor>
         <Routes>
           <Route element={<ScrollToTop />}>
@@ -112,6 +112,21 @@ function App() {
         <FaInstagram className="subtitle" onClick={() => handleClickLink(LinkType.INSTAGRAM)} />
         <FaLinkedin className="subtitle" onClick={() => handleClickLink(LinkType.LINKEDIN)} />
       </div>
+
+      <footer
+            className='absolute bottom-0 right-0 flex flex-row items-stretch justify-end py-6 px-8 lg:py-8 lg:pr-12 mb-4 gap-28 xl:gap-36 lg:gap-48 zIndex20'>
+            <div className='hidden lg:flex flex-col items-end justify-end flex-1'>
+                <div className="child:subtitle-withouthover child:font-normal child:text-gray-300 flex flex-col items-start justify-center gap-3">
+                    <p>I'm <span className="text-green-500 font-bold">Android & Web</span> Developer</p>
+                    <p><span className="text-purple-500 font-bold">Kotlin</span> & <span className="text-sky-500">React</span></p>
+                </div>
+            </div>
+
+            <div className='child:subtitle child:font-normal child:text-gray-300 hidden md:flex flex-col items-start justify-end gap-3'>
+                <p>E: <span className="font-bold">arashaltafi1377@gmail.com</span></p>
+                <p>T: <span className="font-bold">+98 918 767 7641</span></p>
+            </div>
+        </footer>
     </div>
   );
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,3 @@
-import FooterComponent from "../Components/FooterComponent"
 import HomeComponent from "../Components/HomeComponent"
 import ParticlesComponent, { ParticlesType } from "../Components/ParticlesComponent"
 
@@ -6,11 +5,8 @@ const Home = () => {
 
     return (
         <>
-            <div className="relative index-home h-screen w-full flex justify-center items-center flex-col text-center select-none">
-                <footer className="w-full absolute bottom-0 left-0 right-0 zIndex20">
-                    <FooterComponent />
-                </footer>
-                <main className="w-full h-full flex items-center justify-center text-white flex-1 text-center pb-[160px] zIndex20">
+            <div className="relative index-home h-screen w-full flex justify-center items-center flex-col text-center">
+                <main className="w-full h-full flex items-center justify-center text-white zIndex20">
                     <HomeComponent />
                 </main>
             </div>
