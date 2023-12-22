25ac3afc027c9bec4254d64dd6405b81efc05c7a
Arash Altafi <arashaltafi1377@gmail.com>
2023-12-22 23:00:38 +0330

    a

--- src/pages/Works.tsx
+++ src/pages/Works.tsx
@@ -1,11 +1,26 @@
-import ParticlesComponent, { ParticlesType } from "../Components/ParticlesComponent"
+import { useEffect, useState } from 'react'
+import { CustomMouseMove2 } from '../utils/CustomHoverUtils'
 
 const Works = () => {
-  return (
-    <div>
-      <div>Resume</div>
 
-      <ParticlesComponent particlesType={ParticlesType.Triangles} />
+  //mouse move animation
+  const [mousePosition, setMousePosition] = useState<{ x: number; y: number }>({ x: 0, y: 0 });
+  useEffect(() => {
+    CustomMouseMove2(setMousePosition)
+  }, []);
+
+  return (
+    <div className="relative index-works h-screen w-full flex justify-center items-center flex-col text-center zIndex20">
+      <div className="flex justify-center items-center text-center">
+        <h1
+          className="title text-8xl text-center"
+          style={{
+            position: 'absolute',
+            top: `calc(40% + ${mousePosition.y}px)`,
+            left: `calc(30% + ${mousePosition.x}px)`,
+          }}>Works</h1>
+      </div>
+
     </div>
   )
 }
