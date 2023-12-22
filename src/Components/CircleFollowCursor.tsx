469de455545d72f4d9df4e6cabdad002a9e66c58
Arash Altafi <arashaltafi1377@gmail.com>
2023-12-22 13:05:05 +0330

    a

--- src/Components/CircleFollowCursor.tsx
+++ src/Components/CircleFollowCursor.tsx
@@ -8,24 +8,36 @@ const CircleFollowCursor = (prop: any) => {
         setPosition({ x: event.clientX, y: event.clientY });
     };
 
+    const handleBeforeUnload = () => {
+        setPosition({ x: 0, y: 0 })
+    };
+
     return (
         <div
+            id='parent'
             className='h-full w-full relative'
             onMouseMove={handleMouseMove}
+            onMouseLeave={handleBeforeUnload}
         >
-            <div
-                className='absolute w-96 h-96 rounded-full -translate-x-1/2 -translate-y-1/2 opacity-50 radial-background-red zIndex10'
-                style={{
-                    top: position.y,
-                    left: position.x,
-                }}
-            />
-            <div
-                className='absolute w-48 h-48 rounded-full -translate-x-1/2 -translate-y-1/2 opacity-50 radial-background-green zIndex10'
-                style={{
-                    top: position.y,
-                    left: position.x,
-                }} />
+            {
+                position.x === 0 && position.y === 0 ? null :
+                    <>
+                        <div
+                            className='absolute w-96 h-96 rounded-full -translate-x-1/2 -translate-y-1/2 opacity-50 radial-background-red zIndex10'
+                            style={{
+                                top: position.y,
+                                left: position.x,
+                            }}
+                        />
+                        <div
+                            className='absolute w-48 h-48 rounded-full -translate-x-1/2 -translate-y-1/2 opacity-50 radial-background-green zIndex10'
+                            style={{
+                                top: position.y,
+                                left: position.x,
+                            }} />
+
+                    </>
+            }
             {prop.children}
         </div>
     );
